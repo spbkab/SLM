ff3bd94e061408d970a99bcacc206b7e803db50c
spbkab <spbkab33@gmail.com>
2025-09-22 22:56:38 +0300

    test_form 2

--- test_form.py
+++ test_form.py
@@ -6,8 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import allure
 
-
+# Fixture для инициализации драйвера браузера
 @pytest.fixture
 def driver():
     options = Options()
@@ -20,34 +21,40 @@ def driver():
     yield driver
     driver.quit()
 
-
+# Тест на успешный логин
+@pytest.mark.smoke
+@allure.title("Successful login test")
+@allure.description("Verify successful authentication using valid credentials.")
 def test_successful_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
 
-
+    # Заполняем поля формы
     driver.find_element(By.ID, "username").send_keys("tomsmith")
     driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
 
-
+    # Кликаем по кнопке входа
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-
+    # Ждём загрузки области Secure Area
     wait = WebDriverWait(driver, 10)
     secure_area_header = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "h2")))
 
-
+    # Проверяем наличие ожидаемого текста
     assert "Secure Area" in secure_area_header.text
     assert "Welcome to the Secure Area" in driver.page_source
 
-
+# Тест на неуспешный логин
+@pytest.mark.regression
+@allure.title("Unsuccessful login test")
+@allure.description("Verify that incorrect credentials lead to an error message.")
 def test_unsuccessful_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
 
-
+    # Заполняем форму неправильными данными
     driver.find_element(By.ID, "username").send_keys("invalid_user")
     driver.find_element(By.ID, "password").send_keys("invalid_password")
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-
+    # Проверяем появление уведомления об ошибке
     flash_element = driver.find_element(By.CLASS_NAME, "flash")
     assert "Your username is invalid!" in flash_element.text
